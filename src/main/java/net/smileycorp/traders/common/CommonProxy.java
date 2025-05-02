d25f5e82dfc75b91e4416dc109b7ac3d7797103d
Smileycorp <callum.j.longbottom@gmail.com>
2025-05-02 14:39:36 +0100

    add trader spawning

--- src/main/java/net/smileycorp/traders/common/CommonProxy.java
+++ src/main/java/net/smileycorp/traders/common/CommonProxy.java
@@ -1,10 +1,13 @@
 package net.smileycorp.traders.common;
 
+import net.minecraft.world.WorldServer;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.smileycorp.traders.config.TradeDataLoader;
 import net.smileycorp.traders.config.condition.ConditionRegistry;
 import net.smileycorp.traders.config.item.functions.FunctionRegistry;
@@ -30,6 +33,14 @@ public class CommonProxy {
 	
 	public void serverStart(FMLServerStartingEvent event) {
 
+	}
+	
+	@SubscribeEvent
+	public static void worldTick(TickEvent.WorldTickEvent event) {
+		if (event.phase != TickEvent.Phase.END | !(event.world instanceof WorldServer)) return;
+		WorldServer world = (WorldServer) event.world;
+		if (world.provider.getDimension() != 0) return;
+		WanderingTraderSpawner.getSpawner(world).tick(world);
 	}
 	
 }
---
+++ src/main/java/net/smileycorp/traders/common/WanderingTraderSpawner.java
@@ -0,0 +1,122 @@
+package net.smileycorp.traders.common;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.village.Village;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.storage.WorldSavedData;
+import net.smileycorp.traders.common.entities.EntityTraderLlama;
+import net.smileycorp.traders.common.entities.EntityWanderingTrader;
+
+public class WanderingTraderSpawner {
+    
+    private final WorldData data;
+    private int tickDelay;
+    
+    public WanderingTraderSpawner(WorldData data) {
+        this.data = data;
+        tickDelay = 1200;
+    }
+    
+    public void tick(WorldServer world) {
+        if (tickDelay-- > 0) return;
+        tickDelay = 1200;
+        data.spawnDelay -= 1200;
+        if (data.spawnDelay > 0) return;
+        data.spawnDelay = 24000;
+        if (!world.getGameRules().getBoolean("doMobSpawning")) return;
+        if (world.rand.nextInt(100) > data.spawnChance) if (spawn(world)) {
+            data.spawnChance = 25;
+            return;
+        }
+        data.spawnChance = MathHelper.clamp(data.spawnChance + 25, 25, 75);
+    }
+    
+    private boolean spawn(WorldServer world) {
+        if (world.rand.nextInt(10) != 0) return false;
+        EntityPlayer player = world.playerEntities.get(world.rand.nextInt(world.playerEntities.size()));
+        BlockPos pos = player.getPosition();
+        Village village = world.getVillageCollection().getNearestVillage(pos, 48);
+        if (village != null) pos = village.getCenter();
+        BlockPos start = findSpawnPosition(world, pos, 48);
+        if (world.collidesWithAnyBlock(new AxisAlignedBB(pos, pos.add(1, 2, 1)))) return false;
+        EntityWanderingTrader trader = new EntityWanderingTrader(world);
+        trader.setPosition(start.getX() + 0.5f, start.getY(), start.getZ() + 0.5f);
+        if (!world.spawnEntity(trader)) return false;
+        for (int i = 0; i < 2; i++) spawnLlama(world, trader);
+        trader.setHomePosAndDistance(pos, 16);
+        trader.setWanderTarget(pos);
+        trader.setDespawnDelay(48000);
+        return true;
+    }
+    
+    private void spawnLlama(WorldServer world, EntityWanderingTrader trader) {
+        BlockPos pos = findSpawnPosition(world, trader.getPosition(), 4);
+        EntityTraderLlama llama = new EntityTraderLlama(world);
+        llama.setPosition(pos.getX() + 0.5f, pos.getY(), pos.getZ() + 0.5f);
+        world.spawnEntity(llama);
+        llama.setLeashHolder(trader, true);
+    }
+    
+    private BlockPos findSpawnPosition(WorldServer world, BlockPos pos, int range) {
+        for (int i = 0; i < 10; i++) {
+            int x = pos.getX() + world.rand.nextInt(range * 2) - range;
+            int z = pos.getZ() + world.rand.nextInt(range * 2) - range;
+            BlockPos pos1 = new BlockPos(x, world.getHeight(x, z), z);
+            if (world.getBlockState(pos.down()).isNormalCube()) return pos1;
+        }
+        return pos;
+    }
+    
+    public static WanderingTraderSpawner getSpawner(WorldServer world) {
+        return WorldData.getData(world).getSpawner();
+    }
+    
+    public static class WorldData extends WorldSavedData {
+        
+        public static final String DATA = "wandering_traders";
+        private WanderingTraderSpawner spawner;
+        private int spawnDelay, spawnChance;
+        
+        public WorldData() {
+            this(DATA);
+        }
+        
+        public WorldData(String data) {
+            super(data);
+            spawnDelay = 24000;
+            spawnChance = 25;
+        }
+        
+        public WanderingTraderSpawner getSpawner() {
+            if (spawner == null) spawner = new WanderingTraderSpawner(this);
+            return spawner;
+        }
+        
+        @Override
+        public void readFromNBT(NBTTagCompound nbt) {
+            spawnDelay = nbt.getInteger("spawnDelay");
+            spawnChance = nbt.getInteger("spawnChance");
+        }
+        
+        @Override
+        public NBTTagCompound writeToNBT(NBTTagCompound compound) {
+            compound.setInteger("spawnDelay", spawnDelay);
+            compound.setInteger("spawnChance", spawnChance);
+            return compound;
+        }
+        
+        public static WorldData getData(WorldServer world) {
+            WorldData data = (WorldData) world.getMapStorage().getOrLoadData(WorldData.class, DATA);
+            if (data == null) {
+                data = new WorldData();
+                world.getMapStorage().setData(DATA, data);
+            }
+            return data;
+        }
+        
+    }
+}
