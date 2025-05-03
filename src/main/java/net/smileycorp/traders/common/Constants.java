2449e4c51b77b3ed8ab0f30de97d2b4b0d665b40
Smileycorp <callum.j.longbottom@gmail.com>
2025-05-03 20:41:22 +0100

    update mod name

--- src/main/java/net/smileycorp/traders/common/Constants.java
+++ src/main/java/net/smileycorp/traders/common/Constants.java
@@ -4,7 +4,7 @@ import net.minecraft.util.ResourceLocation;
 
 public class Constants {
 	
-	public static final String NAME = "Traders";
+	public static final String NAME = "Traders Backport";
 	public static final String MODID = "traders";
 	public static final String VERSION = "1.0.0";
 	public static final String DEPENDENCIES = "";
