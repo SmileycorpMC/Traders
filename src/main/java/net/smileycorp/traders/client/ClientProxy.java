ff574a6cf4311c95b0c9fdfce4738e35f9de895e
Smileycorp <callum.j.longbottom@gmail.com>
2025-05-03 19:58:47 +0100

    add client config

--- src/main/java/net/smileycorp/traders/client/ClientProxy.java
+++ src/main/java/net/smileycorp/traders/client/ClientProxy.java
@@ -1,6 +1,9 @@
 package net.smileycorp.traders.client;
 
+import net.minecraft.client.audio.ISound;
+import net.minecraft.client.audio.PositionedSound;
 import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -14,6 +17,7 @@ import net.smileycorp.traders.common.CommonProxy;
 import net.smileycorp.traders.common.Constants;
 import net.smileycorp.traders.common.entities.EntityTraderLlama;
 import net.smileycorp.traders.common.entities.EntityWanderingTrader;
+import net.smileycorp.traders.config.ClientConfig;
 
 @EventBusSubscriber(value = Side.CLIENT, modid= Constants.MODID)
 public class ClientProxy extends CommonProxy {
@@ -21,6 +25,7 @@ public class ClientProxy extends CommonProxy {
 	@Override
 	public void preInit(FMLPreInitializationEvent event) {
 		super.preInit(event);
+		ClientConfig.syncConfig(event);
 	}
 	
 	@Override
@@ -37,6 +42,20 @@ public class ClientProxy extends CommonProxy {
 	public static void registerModels(ModelRegistryEvent event) {
 		RenderingRegistry.registerEntityRenderingHandler(EntityWanderingTrader.class, RenderWanderingTrader::new);
 		RenderingRegistry.registerEntityRenderingHandler(EntityTraderLlama.class, RenderTraderLLama::new);
+	}
+	
+	@SubscribeEvent
+	public static void playSound(PlaySoundEvent event) {
+		ISound sound = event.getSound();
+		if (sound == null) return;
+		if (!Constants.MODID.equals(sound.getSoundLocation().getResourceDomain()))
+		if (ClientConfig.silentTraders) {
+			event.setResultSound(null);
+			return;
+		}
+		if (!(sound instanceof PositionedSound)) return;
+		((PositionedSound)sound).volume = sound.getVolume() * ClientConfig.traderVolume;
+		event.setResultSound(sound);
 	}
 	
 }
---
+++ src/main/java/net/smileycorp/traders/config/ClientConfig.java
@@ -0,0 +1,25 @@
+package net.smileycorp.traders.config;
+
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+import java.io.File;
+
+public class ClientConfig {
+    
+    public static boolean silentTraders;
+    public static float traderVolume;
+    
+    public static void syncConfig(FMLPreInitializationEvent event) {
+        Configuration config = new Configuration(new File(event.getModConfigurationDirectory().getPath() + "/traders/client.cfg"));
+        try {
+            silentTraders = config.getBoolean("silentTraders", "Client", true,
+                    "Should traders make noise?");
+            traderVolume = config.getFloat("traderVolume", "Client", 1, 0, 10,
+                    "How loud should trader sounds play? (1 is default volume.)");
+        } catch(Exception e) {} finally {
+            if (config.hasChanged()) config.save();
+        }
+    }
+    
+}
